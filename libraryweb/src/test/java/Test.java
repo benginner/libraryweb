651e0dc01c44f7c35fc55850808c82173d307a1b
admin <2419046432@qq.com>
2020-10-26 09:52:06 +0800

    提交library项目到git

---
+++ libraryweb/src/test/java/Test.java
@@ -0,0 +1,21 @@
+import dao.BookDao;
+import dao.UserDao;
+import pojo.Book;
+import pojo.PageBean;
+import pojo.User;
+import service.BookService;
+
+import java.util.List;
+
+/**
+ * @program: libraryweb
+ * 描述：
+ * @author: LJQ
+ * @create: 2020-10-15 11:43
+ **/
+public class Test<T> {
+    public static void main(String[] args) {
+        Test<String> t1=new Test<String>();
+    }
+
+}
